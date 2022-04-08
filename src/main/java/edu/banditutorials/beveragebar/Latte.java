864d7d3a241f4adeb59feaabd7cc0381ec6f7d84
yogi4297 <71748169+yogi4297@users.noreply.github.com>
2022-04-08 10:41:02 -0500

    Modified the existing Coffee class to abstract class and added abstract method,getters and setters, created new class which extends Coffee class

---
+++ src/main/java/edu/banditutorials/beveragebar/Latte.java
@@ -0,0 +1,79 @@
+package edu.banditutorials.beveragebar;
+
+/**
+ *
+ * @author YogaNarasimhaReddy Peddireddy
+ */
+public class Latte extends Coffee {
+    
+    private static final double MILKCOSTPERSERVING=1.25;
+    private static final double WATERCOSTPERSERVING=1;
+    private static final double BEANSCOSTPERSERVING=11.85;
+    private static final double SUGARCOSTPERSERVING=2;
+    private int quantity;
+    
+
+    public Latte(int quantity, Boolean isServeredHot, double caffeine,String name) {
+        super(isServeredHot, caffeine, name);
+        this.quantity = quantity;
+    }
+
+    
+    
+    
+    @Override
+    public double calcPrice() {       
+        
+        return (MILKCOSTPERSERVING+WATERCOSTPERSERVING+BEANSCOSTPERSERVING+SUGARCOSTPERSERVING)*quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public static double getMILKCOSTPERSERVING() {
+        return MILKCOSTPERSERVING;
+    }
+
+    public static double getWATERCOSTPERSERVING() {
+        return WATERCOSTPERSERVING;
+    }
+
+    public static double getBEANSCOSTPERSERVING() {
+        return BEANSCOSTPERSERVING;
+    }
+
+    public static double getSUGARCOSTPERSERVING() {
+        return SUGARCOSTPERSERVING;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        String out;
+        if (super.getIsServeredHot())
+        {   
+            if (quantity>1)
+            out=quantity+" Lattes served hot";
+            else
+            out="one Latte served hot";
+        }
+        else
+        {
+            if (quantity>1)
+            out=quantity+" Lattes served cold";
+            else
+            out="one Latte served cold";
+        }
+        return out;
+    }
+    
+    
+    
+    
+}
